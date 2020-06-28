f89a9c67043c6cec93565b4e9655e7bfbd438005
JuanJGomezA <67541109+JuanJGomezA@users.noreply.github.com>
2020-06-28 05:09:26 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+document.querySelector('.menu-btn').addEventListener('click', () => {
+    document.querySelector('.menu').classList.toggle('show');
+})
+
+ScrollReveal().reveal('.principal');
+ScrollReveal().reveal('.RAZAS',{delay:500});
+ScrollReveal().reveal('.banner_1',{delay:500});
+ScrollReveal().reveal('.banner_2',{delay:500});
